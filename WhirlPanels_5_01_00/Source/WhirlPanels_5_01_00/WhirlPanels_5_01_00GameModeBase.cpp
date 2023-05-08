26bcb879ae7174e70c16fd2e3bf75c6379eba258
klauth86 <kehel@yandex.ru>
2023-05-08 19:18:44 +0200

    initial commit

---
+++ WhirlPanels_5_01_00/Source/WhirlPanels_5_01_00/WhirlPanels_5_01_00GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "WhirlPanels_5_01_00GameModeBase.h"
+
---
+++ WhirlPanels_5_01_00/Source/WhirlPanels_5_01_00/WhirlPanels_5_01_00GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WhirlPanels_5_01_00GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHIRLPANELS_5_01_00_API AWhirlPanels_5_01_00GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
