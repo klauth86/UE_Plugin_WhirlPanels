d1cc54c6b2fdd653a1e5251e43630729b91c76d3
klauth86 <kehel@yandex.ru>
2023-05-24 19:09:15 +0200

    Add project source: PaginatorPanel

---
+++ WhirlPanels_5_02_00/Plugins/WhirlPanels/Source/WhirlPanels/Private/Components/PaginatorPanel.cpp
@@ -0,0 +1,305 @@
+// Copyright 2023 Pentangle Studio Licensed under the Unlicense License (the «LICENSE»);
+
+#pragma once
+
+#include "Components/PaginatorPanel.h"
+#include "Layout/ArrangedChildren.h"
+#include "Layout/LayoutUtils.h"
+
+#define LOCTEXT_NAMESPACE "WHIRLPANELS_SPaginatorPanel"
+
+float SPaginatorPanel::RecalcTarget(const TAttribute<int32>& deltaTargetIndex)
+{
+	if (TargetAlpha == 0)
+	{
+		TargetIndex = TargetIndex + deltaTargetIndex.Get(0);
+	}
+	else
+	{
+		const int newTargetIndex = TargetIndex + deltaTargetIndex.Get(0);
+		float newTargetAlpha = (TargetIndex - StartIndex) * TargetAlpha / (TargetIndex - StartIndex + deltaTargetIndex.Get(0));
+
+		TargetIndex = newTargetIndex;
+		TargetAlpha = newTargetAlpha;
+	}
+
+	return TargetAlpha;
+}
+
+void SPaginatorPanel::Construct(const FArguments& InArgs)
+{
+	Slots.AddSlots(MoveTemp(const_cast<TArray<FSlot::FSlotArguments>&>(InArgs._Slots)));
+	SetStartIndex(InArgs._StartIndex);
+	SetCount(InArgs._Count);
+	SetItemSize(InArgs._ItemSize);
+	SetDirection(InArgs._Direction);
+	SetTargetIndex(InArgs._TargetIndex);
+	SetTargetAlpha(InArgs._TargetAlpha);
+}
+
+void SPaginatorPanel::OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const
+{
+	const float deltaIndex = (TargetIndex - StartIndex) * TargetAlpha;
+	const float mixedIndex = StartIndex + deltaIndex;
+	const int startIndex = FMath::FloorToInt32(mixedIndex);
+	const int endIndex = FMath::CeilToInt32(mixedIndex) + Count;
+
+	const FVector2D directionStep = Direction * ItemSize;
+
+	for (int32 SlotIndex = 0; SlotIndex < Slots.Num(); ++SlotIndex)
+	{
+		if (SlotIndex >= startIndex && SlotIndex < endIndex)
+		{
+			const FSlot& CurSlot = Slots[SlotIndex];
+			const EVisibility ChildVisibility = CurSlot.GetWidget()->GetVisibility();
+			if (ArrangedChildren.Accepts(ChildVisibility))
+			{
+				const FMargin SlotPadding(CurSlot.GetPadding());
+				AlignmentArrangeResult XAxisResult = AlignChild<Orient_Horizontal>(ItemSize.X, CurSlot, SlotPadding);
+				AlignmentArrangeResult YAxisResult = AlignChild<Orient_Vertical>(ItemSize.Y, CurSlot, SlotPadding);
+
+				ArrangedChildren.AddWidget(ChildVisibility, AllottedGeometry.MakeChild(
+					CurSlot.GetWidget(),
+					(SlotIndex - StartIndex - deltaIndex) * directionStep + FVector2D(SlotPadding.Left, SlotPadding.Top),
+					FVector2D(XAxisResult.Size, YAxisResult.Size)
+				));
+			}
+		}
+	}
+}
+
+UPaginatorPanelSlot::UPaginatorPanelSlot(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Slot = nullptr;
+
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	Padding = FMargin();
+	HorizontalAlignment = HAlign_Fill;
+	VerticalAlignment = VAlign_Fill;
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+FMargin UPaginatorPanelSlot::GetPadding() const
+{
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	return Slot ? Slot->GetPadding() : Padding;
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UPaginatorPanelSlot::SetPadding(FMargin InPadding)
+{
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	Padding = InPadding;
+	if (Slot)
+	{
+		Slot->SetPadding(InPadding);
+	}
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+EHorizontalAlignment UPaginatorPanelSlot::GetHorizontalAlignment() const
+{ 
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	return Slot ? Slot->GetHorizontalAlignment() : HorizontalAlignment.GetValue();
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UPaginatorPanelSlot::SetHorizontalAlignment(EHorizontalAlignment InHorizontalAlignment)
+{
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	HorizontalAlignment = InHorizontalAlignment;
+	if (Slot)
+	{
+		Slot->SetHorizontalAlignment(InHorizontalAlignment);
+	}
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+EVerticalAlignment UPaginatorPanelSlot::GetVerticalAlignment() const
+{	
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	return Slot ? Slot->GetVerticalAlignment() : VerticalAlignment.GetValue();
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UPaginatorPanelSlot::SetVerticalAlignment(EVerticalAlignment InVerticalAlignment)
+{
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	VerticalAlignment = InVerticalAlignment;
+	if (Slot)
+	{
+		Slot->SetVerticalAlignment(InVerticalAlignment);
+	}
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UPaginatorPanelSlot::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	SetHorizontalAlignment(HorizontalAlignment);
+	SetVerticalAlignment(VerticalAlignment);
+	SetPadding(Padding);
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+void UPaginatorPanelSlot::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+
+	Slot = nullptr;
+}
+
+void UPaginatorPanelSlot::BuildSlot(TSharedRef<SPaginatorPanel> InPaginatorPanel)
+{
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+	InPaginatorPanel->AddSlot()
+		.Expose(Slot)
+		.Padding(Padding)
+		.HAlign(HorizontalAlignment)
+		.VAlign(VerticalAlignment)
+		[
+			Content == nullptr ? SNullWidget::NullWidget : Content->TakeWidget()
+		];
+
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+}
+
+UPaginatorPanelWidget::UPaginatorPanelWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bIsVariable = false;
+	SetVisibilityInternal(ESlateVisibility::SelfHitTestInvisible);
+
+	StartIndex = 0;
+	Count = 5;
+	ItemSize = FIntPoint(128, 128);
+	Direction = FVector2D(1, 0);
+	TargetIndex = 0;
+	TargetAlpha = 0;
+}
+
+void UPaginatorPanelWidget::SetStartIndex(int32 startIndex) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetStartIndex(StartIndex = startIndex); }
+
+void UPaginatorPanelWidget::SetCount(int32 count) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetCount(Count = count); }
+
+void UPaginatorPanelWidget::SetItemSize(const FIntPoint& itemSize) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetItemSize(ItemSize = itemSize); }
+
+void UPaginatorPanelWidget::SetDirection(const FVector2D& direction) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetDirection(Direction = direction); }
+
+void UPaginatorPanelWidget::SetTargetIndex(int32 targetIndex) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetTargetIndex(TargetIndex = targetIndex); }
+
+void UPaginatorPanelWidget::SetTargetAlpha(float targetAlpha) { if (MyPaginatorPanel.IsValid()) MyPaginatorPanel->SetTargetAlpha(TargetAlpha = targetAlpha); }
+
+float UPaginatorPanelWidget::RecalcTarget(int32 deltaTargetIndex)
+{
+	float recalcedTargetAlpha = MyPaginatorPanel.IsValid() ? MyPaginatorPanel->RecalcTarget(deltaTargetIndex) : INDEX_NONE;
+	TargetIndex = MyPaginatorPanel.IsValid() ? MyPaginatorPanel->GetTargetIndex() : TargetIndex;
+	return recalcedTargetAlpha;
+}
+
+void UPaginatorPanelWidget::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	if (MyPaginatorPanel.IsValid())
+	{
+		MyPaginatorPanel->SetStartIndex(StartIndex);
+
+		MyPaginatorPanel->SetCount(Count);
+
+		MyPaginatorPanel->SetItemSize(ItemSize);
+
+		MyPaginatorPanel->SetDirection(Direction);
+
+		MyPaginatorPanel->SetTargetIndex(TargetIndex);
+
+		MyPaginatorPanel->SetTargetAlpha(TargetAlpha);
+	}
+}
+
+void UPaginatorPanelWidget::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+
+	MyPaginatorPanel.Reset();
+}
+
+UPaginatorPanelSlot* UPaginatorPanelWidget::AddChildToPaginatorPanel(UWidget* Content) { return Cast<UPaginatorPanelSlot>(Super::AddChild(Content)); }
+
+UPaginatorPanelSlot* UPaginatorPanelWidget::AddChildToPaginatorPanelAt(UWidget* Content, int32 index) { return Cast<UPaginatorPanelSlot>(Super::InsertChildAt(index, Content)); }
+
+UClass* UPaginatorPanelWidget::GetSlotClass() const { return UPaginatorPanelSlot::StaticClass(); }
+
+void UPaginatorPanelWidget::OnSlotAdded(UPanelSlot* InSlot)
+{
+	// Add the child to the live canvas if it already exists
+	if (MyPaginatorPanel.IsValid())
+	{
+		CastChecked<UPaginatorPanelSlot>(InSlot)->BuildSlot(MyPaginatorPanel.ToSharedRef());
+	}
+}
+
+void UPaginatorPanelWidget::OnSlotRemoved(UPanelSlot* InSlot)
+{
+	// Remove the widget from the live slot if it exists.
+	if (MyPaginatorPanel.IsValid() && InSlot->Content)
+	{
+		TSharedPtr<SWidget> Widget = InSlot->Content->GetCachedWidget();
+		if (Widget.IsValid())
+		{
+			MyPaginatorPanel->RemoveSlot(Widget.ToSharedRef());
+		}
+	}
+}
+
+TSharedRef<SWidget> UPaginatorPanelWidget::RebuildWidget()
+{
+	MyPaginatorPanel = SNew(SPaginatorPanel)
+		.StartIndex(StartIndex)
+		.Count(Count)
+		.ItemSize(ItemSize)
+		.Direction(Direction)
+		.TargetIndex(TargetIndex)
+		.TargetAlpha(TargetAlpha);
+
+	for (UPanelSlot* PanelSlot : Slots)
+	{
+		if (UPaginatorPanelSlot* TypedSlot = Cast<UPaginatorPanelSlot>(PanelSlot))
+		{
+			TypedSlot->Parent = this;
+			TypedSlot->BuildSlot(MyPaginatorPanel.ToSharedRef());
+		}
+	}
+
+	return MyPaginatorPanel.ToSharedRef();
+}
+
+#if WITH_EDITOR
+
+const FText UPaginatorPanelWidget::GetPaletteCategory()
+{
+	return LOCTEXT("Panel", "Panel");
+}
+
+#endif
+
+#undef LOCTEXT_NAMESPACE
---
+++ WhirlPanels_5_02_00/Plugins/WhirlPanels/Source/WhirlPanels/Public/Components/PaginatorPanel.h
@@ -0,0 +1,230 @@
+// Copyright 2023 Pentangle Studio Licensed under the Unlicense License (the «LICENSE»);
+
+#pragma once
+
+#include "Components/PanelWidget.h"
+#include "Components/PanelSlot.h"
+#include "Widgets/SPanel.h"
+#include "SlotBase.h"
+#include "Widgets/DeclarativeSyntaxSupport.h"
+#include "Layout/Children.h"
+#include "PaginatorPanel.generated.h"
+
+class SPaginatorPanel : public SPanel
+{
+public:
+
+	class FSlot : public TBasicLayoutWidgetSlot<FSlot>
+	{
+	public:
+
+		FSlot()
+			: TBasicLayoutWidgetSlot<FSlot>(HAlign_Fill, VAlign_Fill)
+		{}
+
+		SLATE_SLOT_BEGIN_ARGS(FSlot, TBasicLayoutWidgetSlot<FSlot>)
+			SLATE_SLOT_END_ARGS()
+
+			void Construct(const FChildren& SlotOwner, FSlotArguments&& InArg)
+		{
+			TBasicLayoutWidgetSlot<FSlot>::Construct(SlotOwner, MoveTemp(InArg));
+		}
+	};
+
+	SLATE_BEGIN_ARGS(SPaginatorPanel)
+		: _StartIndex(0)
+		, _Count(5)
+		, _ItemSize(FIntPoint(128, 128))
+		, _Direction(FVector2D(1, 0))
+		, _TargetIndex(0)
+		, _TargetAlpha(0.f)
+	{
+		_Visibility = EVisibility::SelfHitTestInvisible;
+	}
+
+	SLATE_SLOT_ARGUMENT(FSlot, Slots)
+
+		SLATE_ATTRIBUTE(int32, StartIndex)
+
+		SLATE_ATTRIBUTE(int32, Count)
+
+		SLATE_ATTRIBUTE(FIntPoint, ItemSize)
+
+		SLATE_ATTRIBUTE(FVector2D, Direction)
+
+		SLATE_ATTRIBUTE(int32, TargetIndex)
+
+		SLATE_ATTRIBUTE(float, TargetAlpha)
+
+		SLATE_END_ARGS()
+
+		void SetStartIndex(const TAttribute<int32>& startIndex) { StartIndex = startIndex.Get(0); }
+
+	void SetCount(const TAttribute<int32>& count) { Count = count.Get(0); }
+
+	void SetItemSize(const TAttribute<FIntPoint>& itemSize) { ItemSize = itemSize.Get(FIntPoint(128, 128)); }
+
+	void SetDirection(const TAttribute<FVector2D>& direction) { Direction = direction.Get(FVector2D(1, 0)); }
+
+	int32 GetTargetIndex() const { return TargetIndex; }
+
+	void SetTargetIndex(const TAttribute<int32>& targetIndex) { TargetIndex = targetIndex.Get(0); }
+
+	void SetTargetAlpha(const TAttribute<float>& targetAlpha) { TargetAlpha = targetAlpha.Get(0.f); }
+
+	float RecalcTarget(const TAttribute<int32>& deltaTargetIndex);
+
+	SPaginatorPanel() : Slots(this), StartIndex(0), Count(5), ItemSize(FIntPoint(128, 128)), Direction(FVector2D(1, 0)), TargetIndex(0), TargetAlpha(0.f) {}
+
+	void Construct(const FArguments& InArgs);
+
+	using FScopedWidgetSlotArguments = TPanelChildren<FSlot>::FScopedWidgetSlotArguments;
+
+	static FSlot::FSlotArguments Slot() { return FSlot::FSlotArguments(MakeUnique<FSlot>()); }
+	FScopedWidgetSlotArguments AddSlot() { return FScopedWidgetSlotArguments{ MakeUnique<FSlot>(), Slots, INDEX_NONE }; }
+	int32 RemoveSlot(const TSharedRef<SWidget>& SlotWidget) { return Slots.Remove(SlotWidget); }
+
+	virtual void OnArrangeChildren(const FGeometry& AllottedGeometry, FArrangedChildren& ArrangedChildren) const override;
+	virtual FVector2D ComputeDesiredSize(float) const override { return Count > 0 ? FVector2D(Direction.X * (Count - 1) + 1, Direction.Y * (Count - 1) + 1) * ItemSize : FVector2D::ZeroVector; }
+	virtual FChildren* GetChildren() override { return &Slots; }
+
+protected:
+
+	TPanelChildren<FSlot> Slots;
+	int32 StartIndex;
+	int32 Count;
+	FIntPoint ItemSize;
+	FVector2D Direction;
+	int32 TargetIndex;
+	float TargetAlpha;
+};
+
+UCLASS()
+class WHIRLPANELS_API UPaginatorPanelSlot : public UPanelSlot
+{
+	GENERATED_UCLASS_BODY()
+
+protected:
+
+	SPaginatorPanel::FSlot* Slot;
+
+public:
+
+	FMargin GetPadding() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Layout|Border Slot")
+		void SetPadding(FMargin InPadding);
+
+	EHorizontalAlignment GetHorizontalAlignment() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Layout|Grid Slot")
+		void SetHorizontalAlignment(EHorizontalAlignment InHorizontalAlignment);
+
+	EVerticalAlignment GetVerticalAlignment() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Layout|Grid Slot")
+		void SetVerticalAlignment(EVerticalAlignment InVerticalAlignment);
+
+	virtual void SynchronizeProperties() override;
+
+	virtual void BuildSlot(TSharedRef<SPaginatorPanel> InPaginatorPanel);
+
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+protected:
+
+	/** The padding area between the slot and the content it contains. */
+	UE_DEPRECATED(5.1, "Direct access to Padding is deprecated. Please use the getter or setter.")
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Getter, Setter, BlueprintSetter = "SetPadding", Category = "Layout|Grid Slot")
+		FMargin Padding;
+
+	/** The alignment of the object horizontally. */
+	UE_DEPRECATED(5.1, "Direct access to HorizontalAlignment is deprecated. Please use the getter or setter.")
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Getter, Setter, BlueprintSetter = "SetHorizontalAlignment", Category = "Layout|Grid Slot")
+		TEnumAsByte<EHorizontalAlignment> HorizontalAlignment;
+
+	/** The alignment of the object vertically. */
+	UE_DEPRECATED(5.1, "Direct access to VerticalAlignment is deprecated. Please use the getter or setter.")
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Getter, Setter, BlueprintSetter = "SetVerticalAlignment", Category = "Layout|Grid Slot")
+		TEnumAsByte<EVerticalAlignment> VerticalAlignment;
+};
+
+UCLASS()
+class WHIRLPANELS_API UPaginatorPanelWidget : public UPanelWidget
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetStartIndex(int32 startIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetCount(int32 count);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetItemSize(const FIntPoint& itemSize);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetDirection(const FVector2D& direction);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetTargetIndex(int32 targetIndex);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		void SetTargetAlpha(float targetAlpha);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		float RecalcTarget(int32 deltaTargetIndex);
+
+	virtual void SynchronizeProperties() override;
+
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		UPaginatorPanelSlot* AddChildToPaginatorPanel(UWidget* Content);
+
+	UFUNCTION(BlueprintCallable, Category = "Widget")
+		UPaginatorPanelSlot* AddChildToPaginatorPanelAt(UWidget* Content, int32 index);
+
+#if WITH_EDITOR
+	virtual const FText GetPaletteCategory() override;
+#endif
+
+protected:
+
+	// UPanelWidget
+	virtual UClass* GetSlotClass() const override;
+	virtual void OnSlotAdded(UPanelSlot* Slot) override;
+	virtual void OnSlotRemoved(UPanelSlot* Slot) override;
+	// End UPanelWidget
+
+protected:
+
+	TSharedPtr<SPaginatorPanel> MyPaginatorPanel;
+
+protected:
+	// UWidget interface
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	// End of UWidget interface
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel", meta = (UIMin = "0", ClampMin = "0"))
+		int32 StartIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel", meta = (UIMin = "0", ClampMin = "0"))
+		int32 Count;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel", meta = (UIMin = "0", ClampMin = "0"))
+		FIntPoint ItemSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel", meta = (UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
+		FVector2D Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel")
+		int32 TargetIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Paginator Panel", meta = (UIMin = "0", UIMax = "1", ClampMin = "0", ClampMax = "1"))
+		float TargetAlpha;
+};
