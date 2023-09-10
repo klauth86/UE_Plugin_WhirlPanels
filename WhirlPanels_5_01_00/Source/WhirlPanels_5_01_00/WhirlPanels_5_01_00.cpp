042a61f11da5ed973db24aaafd980b212ba11132
klauth86 <kehel@yandex.ru>
2023-09-10 09:41:19 +0200

    Initial commit

--- WhirlPanels_5_01_00/Source/WhirlPanels_5_01_00/WhirlPanels_5_01_00.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WhirlPanels_5_01_00.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WhirlPanels_5_01_00, "WhirlPanels_5_01_00" );
